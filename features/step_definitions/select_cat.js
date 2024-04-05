f040cf0ac875533ff643aabfd8a27f0085b6c311
Jose Cota <89209438+Yuzemait@users.noreply.github.com>
2024-04-05 09:00:17 -0600

    Add files via upload

---
+++ features/step_definitions/select_cat.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('Estoy en la pagina de busqueda de favores', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('Tengo una cuenta de MiApoyoIteso', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('Selecciono de la lista la categoria {string}', function (givenCategoria) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('Veo una lista de favores relacionados con la categoria {string} ', function (givenCategoria) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
