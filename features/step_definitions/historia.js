2bd0314480000d95410fb061fe6a1f3c7ab332d6
DESKTOP-03NAKMJ\Mateo <matehdz150@gmail.com>
2024-04-05 08:13:22 -0600

    fetaures agregados correccion

---
+++ features/step_definitions/historia.js
@@ -0,0 +1,103 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('estoy en la pagina de del perfil del usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('ya tengo cuenta de  MiApoyoITESO', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('Quiero visualizar mi informacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('te permite ver tus datos pesonales', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('estoy en la pagina del perfil del usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Tengo cuenta de MiAPoyoITESO y quiero modificar algo', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('quiero mover informacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Te permite modificar tu informacion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('estoy en la pagina del perfil del usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Quiero salir de esa seccion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('Salgo de la pagina del perfil', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Te permite salir de la pagina dle perfil', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('estoy en la pagina del perfil del usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Tengo cuenta de MiAPoyoITESO y quiero borrarla', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('quiero borrar mi cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Te permite borrar mi cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('estoy en la pagina del perfil del usuario', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('Tengo cuenta de MiAPoyoITESO y necesito ayuda', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('quiero mover informacion o tengo dudas', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Te permite ver a un administrador', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
